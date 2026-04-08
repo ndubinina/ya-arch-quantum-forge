a06b93c8cb70c4c3e3e541880f53d33be19bbdce
Nina <dnv86@yandex.ru>
2026-04-08 19:25:56 +0300

    task 2 done

--- replace_base.py
+++ replace_base.py
@@ -1,11 +1,19 @@
 import json
 import os
+import re
 
 terms_file = "terms_map.json"
 kb_folder = "knowledge_base"
 
 with open(terms_file, "r", encoding="utf-8") as f:
     terms_map = json.load(f)
+
+def replace_match(match, replacement):
+    word = match.group()
+    if word[0].isupper():
+        return replacement.capitalize()
+    else:
+        return replacement.lower()
 
 for filename in os.listdir(kb_folder):
     if filename.endswith(".txt"):
@@ -15,7 +23,9 @@ for filename in os.listdir(kb_folder):
             content = f.read()
 
         for old, new in terms_map.items():
-            content = content.replace(old, new)
+            pattern = re.compile(re.escape(old), re.IGNORECASE)
+            content = pattern.sub(lambda m: replace_match(m, new), content)
 
+        # Сохраняем обратно
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(content)
